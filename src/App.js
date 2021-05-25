5f5260328c1afd707daafc5465904bdf6e2fc0f5
Sparking <sparking02@yahoo.com.mx>
2021-05-24 20:31:45 -0500

    Empty scratch React Project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Hello World!
+      </h1>
+      <div style={{ backgroundColor: 'green' }}>
+        hola
+      </div>
+    </div>
+  );
+}
+export default App;
